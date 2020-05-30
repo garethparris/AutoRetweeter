bc151165b85fefcb588e56fbcd14e4fae3dfa8ef
Gareth Parris <gareth@prime23.co.uk>
2020-05-30 22:06:36 +0100

    Added loading of Twitter keys from docker environment

--- AutoRetweeter/MonitorSettings.cs
+++ AutoRetweeter/MonitorSettings.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 using Prime23.AutoRetweeter.Models;
 
@@ -18,19 +19,59 @@ namespace Prime23.AutoRetweeter
             ' ', ',', ';', '\t'
         };
         private readonly AppSettings appSettings;
+        private readonly ILogger<MonitorSettings> logger;
 
-        public MonitorSettings(IConfiguration configuration)
+        public MonitorSettings(ILogger<MonitorSettings> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            var tagGroupLookup = GetTagGroups(configuration);
+            this.ActionLookup = this.GetActionGroups(configuration, tagGroupLookup);
+
+            this.appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
+
+            this.TwitterSettings = this.GetTwitterSettings(configuration);
+        }
+
+        internal IDictionary<string, TagGroupAction> ActionLookup { get; }
+
+        internal int BatchSize => this.appSettings.BatchSize;
+
+        internal int PostBatchProcessingDelayInSeconds => this.appSettings.PostBatchProcessingDelayInSeconds;
+
+        internal TwitterSettings TwitterSettings { get; }
+
+        private static bool InDocker => Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+        private static string GetStatus(string value)
+        {
+            return string.IsNullOrEmpty(value) ? "loaded ok" : "not loaded!";
+        }
+
+        private static Dictionary<string, List<string>> GetTagGroups(IConfiguration configuration)
         {
             var tagGroups = configuration.GetSection("TagGroups").Get<TagGroup[]>();
             var tagGroupLookup = new Dictionary<string, List<string>>();
+
             foreach (var tagGroup in tagGroups)
             {
                 var hashTags = SplitToList(tagGroup.Tags.ToLower());
                 tagGroupLookup.Add(tagGroup.Group, hashTags);
             }
 
+            return tagGroupLookup;
+        }
+
+        private static List<string> SplitToList(string value)
+        {
+            return value.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        private Dictionary<string, TagGroupAction> GetActionGroups(
+            IConfiguration configuration,
+            IReadOnlyDictionary<string, List<string>> tagGroupLookup)
+        {
             var userGroups = configuration.GetSection("UserGroups").Get<UserGroup[]>();
-            this.ActionLookup = new Dictionary<string, TagGroupAction>();
+            var actionLookup = new Dictionary<string, TagGroupAction>();
 
             foreach (var userGroup in userGroups)
             {
@@ -47,25 +88,38 @@ namespace Prime23.AutoRetweeter
                 var userNames = SplitToList(userGroup.Users);
                 foreach (var userName in userNames)
                 {
-                    this.ActionLookup.Add(userName.ToLower(), tagGroupAction);
+                    actionLookup.Add(userName.ToLower(), tagGroupAction);
                 }
             }
 
-            this.appSettings = configuration.GetSection("AppSettings").Get<AppSettings>();
-            this.TwitterSettings = configuration.GetSection("Twitter").Get<TwitterSettings>();
+            return actionLookup;
         }
 
-        public IDictionary<string, TagGroupAction> ActionLookup { get; }
-
-        public int BatchSize => this.appSettings.BatchSize;
-
-        public int PostBatchProcessingDelayInSeconds => this.appSettings.PostBatchProcessingDelayInSeconds;
-
-        public TwitterSettings TwitterSettings { get; }
-
-        private static List<string> SplitToList(string value)
+        private TwitterSettings GetTwitterSettings(IConfiguration configuration)
         {
-            return value.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries).ToList();
+            TwitterSettings twitterSettings;
+
+            if (InDocker)
+            {
+                twitterSettings = new TwitterSettings
+                {
+                    ConsumerKey = Environment.GetEnvironmentVariable("ConsumerKey"),
+                    ConsumerSecret = Environment.GetEnvironmentVariable("ConsumerSecret"),
+                    AccessToken = Environment.GetEnvironmentVariable("AccessToken"),
+                    AccessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret")
+                };
+            }
+            else
+            {
+                twitterSettings = configuration.GetSection("Twitter").Get<TwitterSettings>();
+            }
+
+            this.logger.LogInformation("Twitter Consumer Key {0}", GetStatus(twitterSettings.ConsumerKey));
+            this.logger.LogInformation("Twitter Consumer Secret {0}", GetStatus(twitterSettings.ConsumerSecret));
+            this.logger.LogInformation("Twitter Access Token {0}", GetStatus(twitterSettings.AccessToken));
+            this.logger.LogInformation("Twitter Access Token Secret {0}", GetStatus(twitterSettings.AccessTokenSecret));
+
+            return twitterSettings;
         }
     }
 }
